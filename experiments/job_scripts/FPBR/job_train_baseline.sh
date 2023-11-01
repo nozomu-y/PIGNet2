869e8ce81219b7943a326c9592ad9f093ffafa31
Nozomu Yamazaki <46639706+nozomu-y@users.noreply.github.com>
2023-11-01 15:02:13 +0900

    feat: train the model on FPBR dataset

---
+++ experiments/job_scripts/FPBR/job_train_baseline.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#$ -cwd
+#$ -l q_node=1
+#$ -l h_rt=23:00:00
+#$ -N TRAIN_BASELINE
+#$ -o eo_file_train
+#$ -e eo_file_train
+
+. /etc/profile.d/modules.sh
+source /home/8/18B15885/.bashrc
+python --version
+pip freeze
+
+module load cuda/11.1.1 cudnn/8.8.1 gcc/8.3.0-cuda
+module list
+
+export NCCL_IB_DISABLE=1
+
+../../training_scripts/FPBR/baseline.sh
