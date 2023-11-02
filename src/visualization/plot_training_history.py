32328bcbb830ec1e5e03b564f5792d021ca00fb3
Nozomu Yamazaki <46639706+nozomu-y@users.noreply.github.com>
2023-11-02 16:20:32 +0900

    fix: create separate plot for each metrics

--- src/visualization/plot_training_history.py
+++ src/visualization/plot_training_history.py
@@ -1,12 +1,24 @@
 import argparse
 import re
 from io import StringIO
+from pathlib import Path
 
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 
 sns.set(style="darkgrid")
+
+metric_to_label = {
+    "l_scoring": "Scoring Loss",
+    "l_docking": "Docking Loss",
+    "l_cross": "Cross Docking Loss",
+    "l_random": "Random Docking Loss",
+    "l_pda": "Positive Data Augmentation Loss",
+    "l_dvdw": "VDW Loss",
+    "r": "Pearson Correlation",
+    "tau": "Kendall Tau",
+}
 
 
 def extract_table_from_train_log(train_log_path):
@@ -28,47 +40,61 @@ def train_log_to_df(train_log_path):
     return df
 
 
-def plot_training_history(df, save_path):
-    fig, axes = plt.subplots(2, 1, figsize=(8, 12))
+def plot_training_history(df, metric, save_dir):
+    fig, ax = plt.subplots(1, 1, figsize=(8, 6))
 
-    # Plot training and testing scoring loss
+    # Plot training history for the given metric
     sns.lineplot(
         data=df,
         x="epoch",
-        y="train_l_scoring",
+        y="train_" + metric,
         label="Training Set",
-        ax=axes[0],
+        ax=ax,
+        linewidth=0.8,
     )
     sns.lineplot(
         data=df,
         x="epoch",
-        y="test_l_scoring",
-        label="Test Set",
-        ax=axes[0],
+        y="val_" + metric,
+        label="Validation Set",
+        ax=ax,
+        linewidth=0.8,
     )
-    axes[0].set_xlabel("Epoch")
-    axes[0].set_ylabel("Scoring Loss")
-
-    # Plot training and testing Pearson correlation coefficient
-    sns.lineplot(data=df, x="epoch", y="train_r", label="Training Set", ax=axes[1])
-    sns.lineplot(data=df, x="epoch", y="test_r", label="Test Set", ax=axes[1])
-    axes[1].set_xlabel("Epoch")
-    axes[1].set_ylabel("Pearson Correlation")
-
-    # Add titles
-    axes[0].set_title("Scoring Loss")
-    axes[1].set_title("Pearson Correlation Coefficient")
+    sns.lineplot(
+        data=df,
+        x="epoch",
+        y="test_" + metric,
+        label="Test Set",
+        ax=ax,
+        linewidth=0.8,
+    )
+    ax.set_xlabel("Epoch")
+    ax.set_ylabel(metric_to_label[metric])
+    ax.set_title(metric_to_label[metric])
 
     plt.tight_layout()
+    save_path = save_dir + "/training_history_" + metric + ".png"
     plt.savefig(save_path, dpi=300)
+
+
+def plot_training_histories(df, save_dir):
+    Path(save_dir).mkdir(parents=True, exist_ok=True)
+    metrics = list(df.columns[1:])
+    metrics = [metric[6:] for metric in metrics if metric.startswith("train")]
+    for metric in metrics:
+        print("Plotting training history for " + metric)
+        plot_training_history(df, metric, save_dir)
+    pass
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("train_log_path", type=str, help="Path to train log file")
+    parser.add_argument(
+        "training_output_dir", type=str, help="Path to training output directory"
+    )
     args = parser.parse_args()
 
-    df = train_log_to_df(args.train_log_path)
-    print(df)
-    save_path = args.train_log_path.replace("train.log", "train_history.png")
-    plot_training_history(df, save_path)
+    train_log_path = args.training_output_dir + "/train.log"
+    df = train_log_to_df(train_log_path)
+    save_dir = args.training_output_dir + "/figures"
+    plot_training_histories(df, save_dir)
